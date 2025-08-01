dc38bf86d24e99c20455ef1e461b188cced60a99
Rim <yelim12337@gmail.com>
2025-08-02 04:38:34 +0900

    state 추가
    어렵다ㅠㅠ

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function Header(props) {
     return <header>
@@ -16,7 +17,7 @@ function Nav(props) {
         lis.push(<li key={t.id}>
             <a id = {t.id} href={'/read/' + t.id} onClick={(event) => {
                 event.preventDefault();
-                props.onChangeMode(event.target.id);
+                props.onChangeMode(Number(event.target.id));
             }}>{t.title}</a>
         </li>)
     }
@@ -33,20 +34,43 @@ function Article(props) {
     </article>
 }
 function App() {
+    // const _mode = useState("WELCOME");
+    // const mode = _mode[0];
+    // const setMode = _mode[1];
+
+    // 더 축약형
+    const [mode, setMode] = useState("WELCOME");
+    const [id, setId] = useState(null);
     const topics = [
         {id:1, title:'html', body:'html is ...'},
         {id:2, title:'css', body:'css is ...'},
         {id:3, title:'javascript', body:'javascript is ...'}
     ]
+    let content = null;
+    if (mode === "WELCOME") {
+        content = <Article title = "Welcome" body = "Hello, WEB"></Article>
+    } else if(mode === "READ") {
+        let title, body = null;
+        for(let i=0; i<topics.length; i++){
+            // 디버깅??
+            console.log(topics[i].id, id);
+            if(topics[i].id === id){
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Article title = {title} body = {body}></Article>
+    }
     return (
         <div>
             <Header title = "WEB" onChangeMode = {() => {
-                alert('Header');
+                setMode("WELCOME");
             }}></Header>
-            <Nav topics = {topics} onChangeMode = {(id) => {
-                alert(id);
+            <Nav topics = {topics} onChangeMode = {(_id) => {
+                setMode("READ");
+                setId(_id);
             }}></Nav>
-            <Article title = "Welcome" body = "Hello, WEB"></Article>
+            {content}
         </div>
     );
 }
