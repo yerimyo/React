52dca0c18ef3ca996890e838260386db8d5e8afa
Rim <yelim12337@gmail.com>
2025-08-02 04:10:35 +0900

    이벤트 추가

--- App.js
+++ App.js
@@ -3,14 +3,22 @@ import './App.css';
 
 function Header(props) {
     return <header>
-        <h1><a href='/'>{props.title}</a></h1>
+        <h1><a href='/' onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+        }}>{props.title}</a></h1>
     </header>
 }
 function Nav(props) {
     const lis = [];
     for(let i=0; i<props.topics.length; i++){
         let t = props.topics[i];
-        lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+        lis.push(<li key={t.id}>
+            <a id = {t.id} href={'/read/' + t.id} onClick={(event) => {
+                event.preventDefault();
+                props.onChangeMode(event.target.id);
+            }}>{t.title}</a>
+        </li>)
     }
     return <nav>
         <ol>
@@ -32,8 +40,12 @@ function App() {
     ]
     return (
         <div>
-            <Header title = "WEB"></Header>
-            <Nav topics = {topics}></Nav>
+            <Header title = "WEB" onChangeMode = {() => {
+                alert('Header');
+            }}></Header>
+            <Nav topics = {topics} onChangeMode = {(id) => {
+                alert(id);
+            }}></Nav>
             <Article title = "Welcome" body = "Hello, WEB"></Article>
         </div>
     );
